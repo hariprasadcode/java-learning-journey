0769b04685d600fc067a55952337feffaa47f5e7
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-02 13:03:51 +0530

    Add files via upload

---
+++ 01_BASICS/Employee.java
@@ -0,0 +1,11 @@
+class Employee
+{
+  // Non static variables inside a class
+  int id=101;
+  String firstName="KL";
+  String lastName="Rahul";
+  String email="klrahul01@gmail.com";
+  double salary=200000.00d;
+  String designation="Player";
+ 
+}

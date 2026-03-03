9f8f7a7e37fac65f978504e183ce92053cb4ff47
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-03 22:24:25 +0530

    Add files via upload
    Creating 2 different objects in single class and reinitializing values only for 2nd onject

---
+++ 01_BASICS/Workers.java
@@ -0,0 +1,30 @@
+class Workers
+{
+      int id=101;
+      String designation="painter";
+      long salary=30000;
+
+            public static void main(String[] args)
+                 {
+                        Workers w1=new Workers();
+ 
+                        System.out.println("Id : "+w1.id);
+                        System.out.println("Designation: "+w1.designation);
+                        System.out.println("Salary : "+w1.salary);
+
+                        Workers w2=new Worker();
+                         
+                         w2.id=102;
+                         w2.designation="carpenter";
+                         w2.salary=40000;
+
+                        System.out.println("Id : "+w2.id);
+                        System.out.println("Designation: "+w2.designation);
+                        System.out.println("Salary : "+w2.salary);
+
+
+ 
+
+ 
+                 }
+}

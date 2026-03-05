4d0c10af1fbad09617d7dd23ff53cc487e941437
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-05 16:41:22 +0530

    With ARUGUMENT Without RRTURMN STATEMENT

---
+++ 01_BASICS/MethodExample2.java
@@ -0,0 +1,16 @@
+package demo1;
+
+class MethodExample2 {
+            //1 with argument without return statement
+	public void add(int a,int b) {
+		int sum=a+b;
+		System.out.println("The Sum of"+a +" and " +b+" ="+ sum);
+	}
+	public static void main(String[] args)
+	{
+		    MethodExample2 me2 = new MethodExample2();
+	        
+		    me2.add(20, 40);
+	}
+	
+}

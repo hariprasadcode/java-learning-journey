fd0c2d16e05abb7b86cc7e92dc3633aa77b70cee
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-09 15:37:09 +0530

    Method overloading example

---
+++ 01_BASICS/Methhod overloading EXAMPLE/App.java
@@ -0,0 +1,18 @@
+package com;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ShapeAreaCalculator sac = new ShapeAreaCalculator();
+
+		sac.area(9.4);
+
+		sac.area(5.8, 4);
+
+		sac.area(15, 79);
+
+		sac.area(34);
+	}
+
+}

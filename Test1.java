9cc1f5b3868a9e9726b0d3b9b03867540dc99110
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-10 11:50:08 +0530

    Scanner

---
+++ Test1.java
@@ -0,0 +1,19 @@
+package com;
+
+import java.util.Scanner;
+
+class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("START");
+
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a + b);
+
+		System.out.println("END");
+
+	}
+
+}

cac6507050514fda93d9673fc4f208c3e63a0686
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-12 12:59:07 +0530

    Example for static

--- Static/Employee.java
+++ Static/Employee.java
@@ -15,10 +15,11 @@ public class Employee {
 	public static void main(String[] args) {
 		
 		// no need to create an object
-		System.out.println(Employee.age);
-		System.out.println(Employee.name);
+		System.out.println(age);   // only if its in same class
+		System.out.println(Employee.name);  // if in same class and if in another class
 		// no need to create an object to call a method , as it is a static method
-		work();
+		work();// only if its in same class
+		Employee.work();// in same class and if in another class
 		
 	}
 

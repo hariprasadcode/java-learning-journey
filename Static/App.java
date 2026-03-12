0bf6cb075f4b51e6f826dcc992785d8f0b34551c
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-12 12:59:54 +0530

    Static example when calling from diff class

---
+++ Static/App.java
@@ -0,0 +1,13 @@
+package com;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Employee.age);// if calling from another class
+		System.out.println(Employee.name);// if calling from another class
+		Employee.work();// if calling from another class
+		
+	}
+
+}

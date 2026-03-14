0c5642856e762f50cf49b4d60ed29448f2658e10
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-14 14:16:03 +0530

    Exampl 1for Constructor

---
+++ Bike.java
@@ -0,0 +1,11 @@
+package dom;
+// example for Constructor. 
+public class Bike {
+
+	public Bike() {
+		System.out.println("Inside a Constructor");
+	              }
+    public static void main(String[] args) {
+		Bike b = new Bike();	
+	}
+}

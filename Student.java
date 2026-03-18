789002b1f2a2dcb83b90e8e9fdfb0a5a47602fbe
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-18 16:49:16 +0530

    Single-level inheritance

---
+++ Student.java
@@ -0,0 +1,11 @@
+package single_level;
+
+public class Student extends Person{
+	int id;
+	String college;
+	
+	public void study() {
+		System.out.println("Learing Single-level inheritance");
+	}
+
+}

b2e9610aeddf9c165a98529eeeb22a353ee8f0b3
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-28 14:43:37 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,43 @@
+package com;
+/*Here we learned about the array and its different ways of declaration initializations */
+public class demo {
+	
+	public static void main(String[] args) {
+		//Array declaration
+		int[] age;
+		
+		//Creating Array
+		age= new int[3];
+		
+		//initialization and creation together
+		int[] mark =new int[3];
+		
+		//initialization
+		age[0]=18;
+		age[1]=19;
+		age[2]=19;
+		
+		mark[0]=55;
+		mark[1]=67;
+		mark[2]=80;
+		
+		//Array Declaration , creation , initialization  all Together
+		String[] name = {"jhon","william","robert"} ;
+		
+		
+		//printing 
+		System.out.println(age[0]);
+		System.out.println(age[1]);
+		System.out.println(age[2]);
+		System.out.println();
+		System.out.println(mark[0]);
+		System.out.println(mark[1]);
+		System.out.println(mark[2]);
+		System.out.println();
+		System.out.println(name[0]);
+		System.out.println(name[1]);
+		System.out.println(name[2]);
+		
+	}
+
+}

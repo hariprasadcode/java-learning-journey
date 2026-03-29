0846cc2f6571d7bd3ce64d76a5303a172958e2b0
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-29 16:19:51 +0530

    Add files via upload

---
+++ JavaCrackKit/FibonacciSeries.java
@@ -0,0 +1,14 @@
+package technical_round;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int a=0,b=1;
+		for(int i=1;i<=10;i++) {
+			System.out.println(a+"");
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+
+}

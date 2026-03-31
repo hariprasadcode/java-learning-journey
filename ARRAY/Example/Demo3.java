0d855aebdd081c312971b84389d12073ff3a5f99
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-31 13:22:12 +0530

    Add files via upload

---
+++ ARRAY/Example/Demo3.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		String[] name = {"Shreyas Iyer","Rishab Panth","Bhumra","Shami"};
+		
+		for(String n:name) {
+			System.out.println("name : "+n);
+		}
+	}
+
+}

f850a887eab367ee3f425343c601e77c3d5cc281
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-31 13:30:59 +0530

    Add files via upload

---
+++ ARRAY/Example/Demo4.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		int[] marks= {67,78,98,56};
+		System.out.println("Average : "+(marks[0]+marks[1]+marks[2]+marks[3])/4);
+		
+	}
+}

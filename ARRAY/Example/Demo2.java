4d28fc93a5d8b56c1bd491cd3c9bad918c97c4bd
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-31 13:16:19 +0530

    Add files via upload

---
+++ ARRAY/Example/Demo2.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.util.Scanner;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the marks : ");
+		int[] marks=new int[6];
+		System.out.print("English : ");
+		marks[0]=scan.nextInt();
+		
+		System.out.print("Kannada : ");
+		marks[1]=scan.nextInt();
+		
+		System.out.print("Hindi : ");
+		marks[2]=scan.nextInt();
+		
+		System.out.print("Science : ");
+		marks[3]=scan.nextInt();
+		
+		System.out.print("Maths : ");
+		marks[4]=scan.nextInt();
+		
+		System.out.print("Socal: ");
+		marks[5]=scan.nextInt();
+		
+		double sum=0;
+		for(int m:marks) {
+			sum+=m;
+		}
+		
+		double avg=sum/6;
+		System.out.println("Average : "+avg);
+		
+		
+	}
+
+}

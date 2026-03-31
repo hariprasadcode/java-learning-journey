ca0268fbfe79bef3351e61d81d15cd19b0d8c484
Hari Prasad <hariprasadacharya993@gmail.com>
2026-03-31 13:23:58 +0530

    Example 4

--- ARRAY/Example/Demo3.java
+++ ARRAY/Example/Demo3.java
@@ -1,9 +1,11 @@
+// than using sysout statement multiple times using forEach . 
 package com;
 
 public class Demo3 {
 	public static void main(String[] args) {
 		String[] name = {"Shreyas Iyer","Rishab Panth","Bhumra","Shami"};
-		
+
+		//foeEach to print all the names
 		for(String n:name) {
 			System.out.println("name : "+n);
 		}

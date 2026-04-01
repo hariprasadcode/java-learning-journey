0a389fad1319b8baa8a3d25eeb4f18c5d18d6c86
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-01 22:03:57 +0530

    Public

---
+++ Student.java
@@ -0,0 +1,27 @@
+package com.dcl;
+
+public class Student {
+	// variable with access specifier
+	public int a=10;
+	
+	//constructor with access specifier
+	public Student() {
+		System.out.println("studying core java");
+		
+	}
+	
+	//method with access specifier
+	public void study() {
+		System.out.println("Studying access specifier");
+	}
+	
+	
+	public static void main(String[] args) {
+		Student s1=new Student();
+		System.out.println(s1.a);
+		s1.study();
+		
+	}
+	
+
+}

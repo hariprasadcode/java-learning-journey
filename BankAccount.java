36e56d14363ef169def3e5a5d60fa9cbb12abada
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-03 18:32:27 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,14 @@
+package com.dcl;
+
+public class BankAccount {
+	
+	private double balance;
+	
+	public void setBalance(double balance) {
+		this.balance=balance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+}

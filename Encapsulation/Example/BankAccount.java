b21bcf502063deda0208488ac646cfb0605ece2b
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-03 18:45:41 +0530

    Update BankAccount.java

--- Encapsulation/Example/BankAccount.java
+++ Encapsulation/Example/BankAccount.java
@@ -1,13 +1,14 @@
+/*Example for encapsulation */
 package com.dcl;
-
+//class is public
 public class BankAccount {
-	
+	//private varaibel
 	private double balance;
-	
+	//public setter method
 	public void setBalance(double balance) {
 		this.balance=balance;
 	}
-	
+	//public getter method
 	public double getBalance() {
 		return balance;
 	}

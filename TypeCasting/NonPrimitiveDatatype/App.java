6c8b9383b220ff295aaa5b305eb98b6db3365cdc
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-08 20:19:02 +0530

    Update App.java

--- TypeCasting/NonPrimitiveDatatype/App.java
+++ TypeCasting/NonPrimitiveDatatype/App.java
@@ -3,13 +3,20 @@ package com.dcl;
 public class App {
 	public static void main(String[] args) {
 		
-		//downcasting
+	    // ------------------ UPCASTING ------------------
+        // Creating object of Son but referencing it with Father type
+        // This is called Upcasting (Child -> Parent)
 		Father f=new Son();
+		 // Accessible: Only parent class members
 		System.out.println(f.height);
-		System.out.println(f.age);//not able the asses child class property
+		// Not Accessible: Child class members using parent reference
+		//System.out.println(f.age);
 		
-		//upcasting
+	    // ------------------ DOWNCASTING ------------------
+        // Converting parent reference back to child reference
+        // This is called Downcasting (Parent -> Child)
 		Son s=(Son)f;
+		// Now both parent and child properties are accessible
 		System.out.println(s.height);
 		System.out.println(s.age);
 		

7572a5023c8cdbd6f76f454923cd09a8d5c3683d
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-09 20:01:44 +0530

    Add files via upload

---
+++ TypeCasting/MainApp.java
@@ -0,0 +1,18 @@
+package com;
+
+public class MainApp {
+	public static void main(String[] args) {
+		//creating object of car class
+		Car c= new Car();
+		c.move();
+		c.display();
+		
+		
+		//creating object of Bike Class
+		Bike b = new Bike();
+		b.move();
+		b.display();
+		
+	}
+
+}

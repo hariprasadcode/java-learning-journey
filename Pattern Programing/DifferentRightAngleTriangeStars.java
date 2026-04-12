e7050d34172c51788ec2dbb1475bb8fb87727c18
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-12 12:53:35 +0530

    Add files via upload

---
+++ Pattern Programing/DifferentRightAngleTriangeStars.java
@@ -0,0 +1,14 @@
+package com.dcl;
+
+public class DifferentRightAngleTriangeStars {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=(5-i)+((5-i)+2);j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+	}
+
+}

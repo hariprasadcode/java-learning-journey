7145631eaf5fd0271849b7e39d1cb1c03bef825d
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-12 12:52:04 +0530

    Add files via upload

---
+++ Pattern Programing/RecombinationPattern.java
@@ -0,0 +1,17 @@
+package com.dcl;
+
+public class RecombinationPattern {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=5-i;j++) {
+			
+			System.out.print(" ");
+			
+		}
+		for(int j=1;j<=i+(i-1);j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}

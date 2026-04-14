35a2dd314c616a2fa4e25342dcbaf16ed990a44b
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-14 17:58:13 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+package com.hp;
+//concrete class
+public class Person {
+	
+	//concrete method 
+	public void work() {
+		System.out.println("working");
+	}
+	//abstract method
+	//we cannot have abstract method inside a concrete class
+	public abstract void eat();
+}

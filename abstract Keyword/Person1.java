48b63aa3b0320f722c6bd9e78234631fcdf050d8
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-14 18:02:33 +0530

    Add files via upload

---
+++ abstract Keyword/Person1.java
@@ -0,0 +1,12 @@
+//example to show can have concrete and abstract method inside a abstract class
+package com.hp;
+//abstract class
+public abstract class Person1 {
+	//concrete method
+	public void work() {
+		System.out.println("working");
+	}
+	//abstract method
+	public abstract void eat();
+
+}

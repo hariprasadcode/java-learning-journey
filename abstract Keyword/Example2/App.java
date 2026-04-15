85e3b87dd9f88bb5e472c0bc4bc9f5112815bc86
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-15 18:21:54 +0530

    Add files via upload

---
+++ abstract Keyword/Example2/App.java
@@ -0,0 +1,11 @@
+package com.hp.examples;
+
+public class App {
+	public static void main(String[] args) {
+		ElectricCar ec =new ElectricCar();
+		ec.shiftGare();
+		ec.start();
+		ec.stop();
+	}
+
+}

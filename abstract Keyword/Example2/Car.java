22c719949de013f6a0e5d263fe88621a3dcb5324
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-15 18:21:28 +0530

    Add files via upload

---
+++ abstract Keyword/Example2/Car.java
@@ -0,0 +1,14 @@
+package com.hp.examples;
+/*When a class extend an abstract class we can do 2 things 
+ * 1 we must override all the abstract method 
+ * 2 or we should make the sub class also abstract*/
+
+public abstract class Car extends Vehicle {
+	
+	abstract void stop();
+	
+	//abstract method of vehicle class
+	//concrete method of vehicle class
+
+
+}

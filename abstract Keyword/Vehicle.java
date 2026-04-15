21b08fb605cd0170a0ff9abc17356aa475a30057
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-15 18:20:07 +0530

    Add files via upload

---
+++ abstract Keyword/Vehicle.java
@@ -0,0 +1,15 @@
+package com.hp.examples;
+//abstract class
+//it can have abstract,concrete methods
+public abstract class Vehicle {
+	
+	//abstract method
+	abstract void start();
+	//concrete method
+	void shiftGare() {
+		System.out.println("Shifting gare");
+	}
+	
+	
+
+}

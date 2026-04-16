b11ce4d926e90463ff48a7ac6642bab958c9efcb
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-16 18:00:59 +0530

    Update User.java

--- abstract Keyword/Example3/User.java
+++ abstract Keyword/Example3/User.java
@@ -1,5 +1,6 @@
 package com.hp.example2;
-
+/*Can create a constructor inside a Abstract class
+ * there is concept called constructor chaining */
 public abstract class User {
 	User(int a){
 		System.out.println("user constructor");

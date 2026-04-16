6657abc6120000e99d557009bbd67f8b6e5bfa51
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-16 18:01:32 +0530

    Update Customer.java

--- abstract Keyword/Example3/Customer.java
+++ abstract Keyword/Example3/Customer.java
@@ -1,5 +1,7 @@
 package com.hp.example2;
-
+/*Using supper calling statement to call parent class constructor 
+we must used Super calling statement if there is parameterized 
+constructor in parent class*/
 public class Customer extends User{
 	Customer(){
 		super(2);

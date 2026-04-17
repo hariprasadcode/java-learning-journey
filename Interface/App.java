9dc0b3f7025db08a08afff34a3c8b8d20332cd34
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-17 14:21:19 +0530

    Add files via upload

---
+++ Interface/App.java
@@ -0,0 +1,10 @@
+package com.dcl;
+
+public class App {
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.eat();
+		System.out.println(Person.age);
+	}
+
+}
---
+++ Interface/Employee.java
@@ -0,0 +1,9 @@
+package com.dcl;
+
+public class Employee implements Person {
+	@Override 
+	public void eat() {
+		System.out.println("eating");
+	}
+
+}

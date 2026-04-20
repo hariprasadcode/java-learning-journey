eb8649631c25e8c384e0debd85e242bcb7290acc
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-20 18:55:03 +0530

    Update Son.java

--- Interface/Example3/Son.java
+++ Interface/Example3/Son.java
@@ -1,5 +1,5 @@
 package com.dcl.example2;
-
+/*We cannot extend multiple class but we can implement multiple interface*/
 public class Son implements Dad , Mom {
 	@Override
 	public void instruct() {

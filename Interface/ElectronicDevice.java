b2efaf0961f8a693ee6fe2851eb2c895978389a6
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-20 18:48:45 +0530

    Add files via upload

---
+++ Interface/ElectronicDevice.java
@@ -0,0 +1,12 @@
+package com.dcl.example3;
+
+public class ElectronicDevice {
+
+	void powerOn() {
+		System.out.println("Device in switching ON");
+	}
+	
+	void powerOff() {
+		System.out.println("Device is switching OFF");
+	}
+}

cea411bc235bfb1f3b32077d7d3821b95e2b8399
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-28 20:32:56 +0530

    Add files via upload

---
+++ Single ton class/Example3/User.java
@@ -0,0 +1,8 @@
+package com.example3;
+
+public class User {
+public static void main(String[] args) {
+	PrimeMinister ele=PrimeMinister.pmElected();
+	System.out.println(ele.name+" elected for "+ele.term+"rd term");
+}
+}

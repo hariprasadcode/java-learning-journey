a0ccf0a132e3a32542c5bb69471ec68cf2621bd1
Hari Prasad <hariprasadacharya993@gmail.com>
2026-04-30 19:28:20 +0530

    Add files via upload

---
+++ String Class/StringMethods.java
@@ -0,0 +1,11 @@
+package string_methods;
+
+public class StringMethods {
+	public static void main(String[] args) {
+		/*s1 get de referenced 2 times */
+		String s1="Programing";
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+	}
+
+}

3935ce3cb9ec96547f768923301f469c0dd9a14a
Hari Prasad <hariprasadacharya993@gmail.com>
2026-05-16 19:53:34 +0530

    Add files via upload

---
+++ Collection Framework/Collection/List Interface/LinkedList/Demo4.java
@@ -0,0 +1,22 @@
+package list.linkedlist;
+
+import java.util.ArrayList;
+
+public class Demo4 {
+	public static void main(String[] args) {
+ArrayList al= new ArrayList();
+		
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		System.out.println(al);
+		al.add(1,14);
+		System.out.println(al);
+		al.set(1, 56);
+		System.out.println(al);
+
+
+		
+	}
+
+}

d4a4a0924813bd896a6e32cd19260d4e8ca56f88
Hari Prasad <hariprasadacharya993@gmail.com>
2026-05-27 21:00:59 +0530

    Add files via upload

---
+++ Generics/Student.java
@@ -0,0 +1,19 @@
+package generics;
+
+public class Student {
+	int id;
+	String name;
+	
+	Student(int id, String name){
+		this.id=id;
+		this.name=name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}

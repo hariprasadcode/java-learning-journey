ce32bc04fe81535adb66692cf71687db2fcebc98
Hari Prasad <hariprasadacharya993@gmail.com>
2026-05-28 22:36:56 +0530

    Add files via upload

---
+++ Collection Framework/Collection/List Interface/Demo.java
@@ -0,0 +1,16 @@
+package vector;
+
+import java.util.Vector;
+
+public class Demo {
+	public static void main(String[] args) {
+		Vector v= new Vector();
+		v.add("Java");
+		v.add(10);
+		v.add(99.9);
+		v.add(true);
+		System.out.println(v);
+	
+	}
+
+}
